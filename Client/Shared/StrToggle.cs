248ea7bd0aa08330bfdbcb0ce0c45423258644f9
BambusControl <mojmir.majer@gmail.com>
2021-11-20 15:30:54 +0100

    Trying SASS

---
+++ Client/Shared/StrToggle.cs
@@ -0,0 +1,20 @@
+﻿namespace AviationWebUI.Shared
+{
+    public class StrToggle
+    {
+        // TODO: is this not almost the same as ?
+        private readonly string _value;
+
+        public string Value => IsActive ? _value : string.Empty;
+        public bool IsActive { get; private set; }
+
+        public StrToggle(string value, bool isActive = true)
+        {
+            _value = value;
+            IsActive = isActive;
+        }
+
+        public void Toggle() => IsActive = !IsActive;
+        public override string ToString() => Value;
+    }
+}
